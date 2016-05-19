3dec77622a9f342027c9e81bae6e70a82ba1332c
justinmichaels006 <justin@couchbase.com>
2016-05-19 13:41:59 -0700

    Updated a few test loaders

---
+++ src/main/java/com/couchbase/sampleload/LoadMe5.java
@@ -0,0 +1,31 @@
+package com.couchbase.sampleload;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.document.JsonStringDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+import javafx.event.Event;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
+/**
+ * Created by justin on 5/16/16.
+ */
+public class LoadMe5 {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        CouchbaseCluster cluster = CouchbaseCluster.create("192.168.61.101");
+        final Bucket bucket = cluster.openBucket("testload");
+        int numDocs = 10000;
+
+        JsonObject jsonObject = (JsonObject) bucket.get("CONT").content();
+        CouchbaseEventDaoImpl cb = CouchbaseEventDaoImpl.getInstance();
+
+        for (int i = 0; i < numDocs; i++) {
+            cb.insertEvent(jsonObject, "cl1", 0);
+        }
+
+    }
+}
